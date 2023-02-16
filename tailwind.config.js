85437f641aed0eb524d13e1a71912e3e6b8f03c8
Seyed Sadegh Ataii <cigoler@gmail.com>
2023-02-16 08:34:14 +0000

    old style css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      Cinzel: ["Cinzel", "serif"],
+      CinzelDecorative: ["Cinzel Decorative", "cursive"],
+      Pica: ["IM Fell DW Pica", "serif"],
+      PicaSC: ["IM Fell DW Pica SC", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
